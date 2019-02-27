4edde05190f1565ff6da9341ffac3da3d62b065e
Simonfabisiak2002 <simon.fabisiak@ocsbstudent.ca>
2019-02-27 09:55:46 -0500

    we did sumthing
    yes yes yes eys

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,70 @@ public class Main {
 		System.out.println("How much was paid?");
 		double payment=in.nextDouble();
 		//TODO: Here is where your code goes
+double change = payment-cost;
+System.out.println("change amount: " +change );
+		
+
+
+		int q=(int)(change/100);
+		double r=(change%100);
+
+		System.out.println("Amount of 100 bills exactly :  " +q);
+
+		int q2=(int)(r/50);
+
+		double r2=(r%50); 
+
+		System.out.println("Amount of 50 bills exactly:  " +q2);
+
+		int q3=(int)(r2/20);
+
+		double r3=(r2%20); 
+
+		System.out.println("Amount of 20 bills exactly:  " +q3);
+	
+		int q4=(int)(r3/10);
+
+		double r4=(r3%10); 
+
+		System.out.println("Amount of 10 bills exactly:  " +q4);
+
+		int q5=(int)(r4/5);
+
+		double r5=(r4%5);
+
+		System.out.println("Amount of 5 bills exactly:  " +q5);
+
+		int q6=(int)(r5/2); 
+
+		double r6=(r5%2);
+
+		System.out.println("Exact amount of toonies:  " +q6);
+
+		int q7=(int)(r6/1); 
+
+		double r7=(r6%1);
+
+		System.out.println("Exact amount of loonies:  " +q7);
+
+		int q9=(int)(r7/0.25); 
+
+		double r9=(r7%0.25);
+
+		System.out.println("Exact amount of quarters:  " +q9);
+
+		int q10=(int)(r9/0.10); 
+
+		double r10=(r9%0.10);
+
+		System.out.println("Exact amount of dimes:  " +q10);
+
+		int q11=(int)(r10/0.05); 
+
+		double r11=(r10%0.05);
+
+		System.out.println("Exact amount of nickels:  " +q11);
+
 	}
 
 }
